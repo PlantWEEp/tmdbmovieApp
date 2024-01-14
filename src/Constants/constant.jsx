74be900998118abb23ea819db65d820f91511639
Vaishnav <109400473+PlantWEEp@users.noreply.github.com>
2024-01-14 20:18:24 +0530

    Update constant.jsx

--- src/Constants/constant.jsx
+++ src/Constants/constant.jsx
@@ -1,4 +1,4 @@
 export const baseurl = "https://api.themoviedb.org/3";
-export const API_key = "3b301f6beb72161f7d715a58cfadbdd8";
+export const API_key = "PI_ke";
 export const Image_url = "https://image.tmdb.org/t/p/original/";
 export const imageUrl = "https://image.tmdb.org/t/p/original";
